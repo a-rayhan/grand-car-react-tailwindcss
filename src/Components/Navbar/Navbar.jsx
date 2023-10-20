156518484b7c2686610c7168ba50449325d81441
rayhan <aburayhandh@gmail.com>
2023-10-20 08:29:35 +0600

    Navbar active links implemented

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -22,31 +22,31 @@ const Navbar = () => {
 
                         <ul className={`absolute w-full h-[600px] top-1/3 right-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 bg-[#ebe8fc] p-8 z-50 flex flex-col items-center pt-36 gap-y-6 text-2xl font-medium lg:hidden ${toggleMenu ? 'hidden' : ''}`}>
                             <li>
-                                <NavLink to='/'>
+                                <NavLink to='/' className={({ isActive, isPending }) => isPending ? "" : isActive ? "text-[#7a63f1]" : ""}>
                                     Home
                                 </NavLink>
                             </li>
 
                             <li>
-                                <NavLink to='/shop'>
-                                    Shop
-                                </NavLink>
-                            </li>
-
-                            <li>
-                                <NavLink to='/brand'>
+                                <NavLink to='/brand' className={({ isActive, isPending }) => isPending ? "" : isActive ? "text-[#7a63f1]" : ""}>
                                     Brand
                                 </NavLink>
                             </li>
 
                             <li>
-                                <NavLink to='/addnew'>
+                                <NavLink to='/shop' className={({ isActive, isPending }) => isPending ? "" : isActive ? "text-[#7a63f1]" : ""}>
+                                    Shop
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink to='/addnew' className={({ isActive, isPending }) => isPending ? "" : isActive ? "text-[#7a63f1]" : ""}>
                                     Add New
                                 </NavLink>
                             </li>
 
                             <li>
-                                <NavLink to='/cart'>
+                                <NavLink to='/cart' className={({ isActive, isPending }) => isPending ? "" : isActive ? "text-[#7a63f1]" : ""}>
                                     Carts
                                 </NavLink>
                             </li>
@@ -63,31 +63,31 @@ const Navbar = () => {
 
                     <ul className="hidden lg:flex justify-between items-center gap-8 text-lg font-medium">
                         <li>
-                            <NavLink to='/'>
+                            <NavLink to='/' className={({ isActive, isPending }) => isPending ? "" : isActive ? "text-[#7a63f1]" : ""}>
                                 Home
                             </NavLink>
                         </li>
 
                         <li>
-                            <NavLink to='/shop'>
-                                Shop
-                            </NavLink>
-                        </li>
-
-                        <li>
-                            <NavLink to='/brand'>
+                            <NavLink to='/brand' className={({ isActive, isPending }) => isPending ? "" : isActive ? "text-[#7a63f1]" : ""}>
                                 Brand
                             </NavLink>
                         </li>
 
                         <li>
-                            <NavLink to='/addnew'>
+                            <NavLink to='/shop' className={({ isActive, isPending }) => isPending ? "" : isActive ? "text-[#7a63f1]" : ""}>
+                                Shop
+                            </NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to='/addnew' className={({ isActive, isPending }) => isPending ? "" : isActive ? "text-[#7a63f1]" : ""}>
                                 Add New
                             </NavLink>
                         </li>
 
                         <li>
-                            <NavLink to='/cart'>
+                            <NavLink to='/cart' className={({ isActive, isPending }) => isPending ? "" : isActive ? "text-[#7a63f1]" : ""}>
                                 Carts
                             </NavLink>
                         </li>
